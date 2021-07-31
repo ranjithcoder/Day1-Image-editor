62ab8d7f06e2f852f64707d522586b7f050ad65d
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-07-31 18:50:38 +0530

    issues solved

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const inputs = document.querySelectorAll('.controls input')
+const inputs = document.querySelectorAll('input')
 
 function handleUpdates() {
     const suffix = this.dataset.sizing || '';
     console.log(this.name);
     console.log(this.value+suffix);
-    document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+    const root = document.styleSheets[0].rules[0].style;
+    root.setProperty(`--${this.name}`,this.value+suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change',handleUpdates));
