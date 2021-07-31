b19349899cd2f1fcf2ea3c81b3dc645472b331ed
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-07-31 19:00:29 +0530

    issues resolved

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const inputs = document.querySelectorAll('input')
 
 function handleUpdates() {
-    const suffix = this.dataset.sizing || '';
-    console.log(this.name);
-    console.log(this.value+suffix);
-    const root = document.styleSheets[0].rules[0].style;
-    root.setProperty(`--${this.name}`,this.value+suffix);
+  const suffix = this.dataset.sizing || "";
+  console.log(this.name);
+  console.log(this.value + suffix);
+  //document.documentElement.style.setProperty(`--${this.name}`, this.value+suffix);
+  const root = document.styleSheets[0].rules[0].style;
+  root.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change',handleUpdates));
